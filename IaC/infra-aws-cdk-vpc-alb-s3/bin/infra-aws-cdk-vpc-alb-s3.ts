74645ba27bb28fa5aaeb9a99f0ed00514c2b37bf
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-23 20:22:44 +0000

    ああああああああああ

--- IaC/infra-aws-cdk-vpc-alb-s3/bin/infra-aws-cdk-vpc-alb-s3.ts
+++ IaC/infra-aws-cdk-vpc-alb-s3/bin/infra-aws-cdk-vpc-alb-s3.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { InfraAwsCdkVpcAlbPriFixStack } from '../lib/infra-aws-cdk-vpc-alb-pri-fix-stack';
+import { InfraAwsCdkVpcAlbS3Stack } from '../lib/infra-aws-cdk-vpc-alb-s3-stack';
 
 const app = new cdk.App();
-new InfraAwsCdkVpcAlbPriFixStack(app, 'InfraAwsCdkVpcAlbPriFixStack', {
+new InfraAwsCdkVpcAlbS3Stack(app, 'InfraAwsCdkVpcAlbS3Stack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
--- IaC/infra-aws-cdk-vpc-alb-s3/test/infra-aws-cdk-vpc-alb-s3.test.ts
+++ IaC/infra-aws-cdk-vpc-alb-s3/test/infra-aws-cdk-vpc-alb-s3.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as InfraAwsCdkVpcAlbPriFix from '../lib/infra-aws-cdk-vpc-alb-pri-fix-stack';
+// import * as InfraAwsCdkVpcAlbS3 from '../lib/infra-aws-cdk-vpc-alb-s3-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-aws-cdk-vpc-alb-pri-fix-stack.ts
+// example resource in lib/infra-aws-cdk-vpc-alb-s3-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new InfraAwsCdkVpcAlbPriFix.InfraAwsCdkVpcAlbPriFixStack(app, 'MyTestStack');
+//   const stack = new InfraAwsCdkVpcAlbS3.InfraAwsCdkVpcAlbS3Stack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
