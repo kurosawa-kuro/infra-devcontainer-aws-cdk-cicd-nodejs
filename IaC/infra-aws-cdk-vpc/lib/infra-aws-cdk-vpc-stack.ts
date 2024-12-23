8ab3770bb4f4e09db3b2792c0f91a9c8f46e4f90
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-23 03:48:28 +0000

    Implement VPC and Security Group configuration in AWS CDK stack
    - Added a new VPC configuration with a maximum of 2 availability zones and a public subnet.
    - Created a security group for the Express.js application, allowing inbound HTTP traffic on port 8080 and SSH traffic on port 22.
    - Updated the stack constructor to specify the AWS region for deployment.
    
    These changes enhance the infrastructure setup for the application, ensuring proper network configuration and security settings.

--- IaC/infra-aws-cdk-vpc/lib/infra-aws-cdk-vpc-stack.ts
+++ IaC/infra-aws-cdk-vpc/lib/infra-aws-cdk-vpc-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class InfraAwsCdkVpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      ...props,
+      env: {
+        region: 'ap-northeast-1',
+      },
+    });
 
-    // The code that defines your stack goes here
+    // 接頭句 Cdk-Vpc-JS-Express-EJS-8080
+    const prefix = 'Cdk-Vpc-JS-Express-EJS-8080';
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraAwsCdkVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // VPCデフォルト
+    const vpc = new ec2.Vpc(this, prefix, {
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ],
+    });
+
+    // セキュリティグループ
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'Security group for Express.js application',
+      allowAllOutbound: true,
+    });
+
+    // Port 8080
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8080),
+      'Allow inbound HTTP traffic on port 8080'
+    );
+
+    // Port 22
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow inbound SSH traffic on port 22'
+    );
   }
 }
