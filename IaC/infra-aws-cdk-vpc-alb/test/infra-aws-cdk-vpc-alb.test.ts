06ffb60ac25fb26d32b052135a79134a4d30b4c9
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-23 04:48:32 +0000

    Update README.md to include CDK initialization command for TypeScript

---
+++ IaC/infra-aws-cdk-vpc-alb/test/infra-aws-cdk-vpc-alb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraAwsCdkVpcAlb from '../lib/infra-aws-cdk-vpc-alb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-aws-cdk-vpc-alb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraAwsCdkVpcAlb.InfraAwsCdkVpcAlbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
