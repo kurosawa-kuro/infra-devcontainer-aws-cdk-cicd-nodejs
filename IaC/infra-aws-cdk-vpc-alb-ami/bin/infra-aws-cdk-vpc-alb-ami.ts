f28a049b223e14ea851257f5b4016b847d4c6101
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-25 00:29:09 +0000

    Remove obsolete files and configurations from the infra-aws-cdk-vpc-alb-s3 project
    - Deleted .gitignore, .npmignore, cdk.json, jest.config.js, Makefile, package-lock.json, package.json, README.md, tsconfig.json, and all TypeScript source files related to the CDK stack.
    - This cleanup streamlines the project by removing unnecessary files and configurations, ensuring a cleaner codebase for future development.

--- IaC/infra-aws-cdk-vpc-alb-ami/bin/infra-aws-cdk-vpc-alb-ami.ts
+++ IaC/infra-aws-cdk-vpc-alb-ami/bin/infra-aws-cdk-vpc-alb-ami.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { InfraAwsCdkVpcAlbS3Stack } from '../lib/infra-aws-cdk-vpc-alb-s3-stack';
+import { InfraAwsCdkVpcAlbAmiStack } from '../lib/infra-aws-cdk-vpc-alb-ami-stack';
 
 const app = new cdk.App();
-new InfraAwsCdkVpcAlbS3Stack(app, 'InfraAwsCdkVpcAlbS3Stack', {
+new InfraAwsCdkVpcAlbAmiStack(app, 'InfraAwsCdkVpcAlbAmiStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
--- IaC/infra-aws-cdk-vpc-alb-ami/test/infra-aws-cdk-vpc-alb-ami.test.ts
+++ IaC/infra-aws-cdk-vpc-alb-ami/test/infra-aws-cdk-vpc-alb-ami.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as InfraAwsCdkVpcAlbS3 from '../lib/infra-aws-cdk-vpc-alb-s3-stack';
+// import * as InfraAwsCdkVpcAlbAmi from '../lib/infra-aws-cdk-vpc-alb-ami-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-aws-cdk-vpc-alb-s3-stack.ts
+// example resource in lib/infra-aws-cdk-vpc-alb-ami-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new InfraAwsCdkVpcAlbS3.InfraAwsCdkVpcAlbS3Stack(app, 'MyTestStack');
+//   const stack = new InfraAwsCdkVpcAlbAmi.InfraAwsCdkVpcAlbAmiStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
