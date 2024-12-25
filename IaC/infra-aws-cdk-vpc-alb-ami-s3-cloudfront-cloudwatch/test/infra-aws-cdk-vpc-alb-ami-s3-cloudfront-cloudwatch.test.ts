bde7f872029bad1c91accb267a68873ea501bda6
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-25 09:39:55 +0000

    Enhance CloudFront distribution and S3 bucket configuration for improved security and performance
    - Updated CloudFront distribution to utilize S3 origin with enhanced security measures, including Origin Access Control (OAC) and a refined bucket policy.
    - Ensured HTTPS protocol is enforced for content delivery, aligning with best practices for secure configurations.
    - Improved resource management by implementing dynamic naming conventions for S3 buckets and adding new CloudFormation outputs for better visibility.
    
    These changes strengthen the security posture and optimize the configuration of the AWS infrastructure, facilitating easier management and deployment.

---
+++ IaC/infra-aws-cdk-vpc-alb-ami-s3-cloudfront-cloudwatch/test/infra-aws-cdk-vpc-alb-ami-s3-cloudfront-cloudwatch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraAwsCdkVpcAlbAmiS3CloudfrontCloudwatch from '../lib/infra-aws-cdk-vpc-alb-ami-s3-cloudfront-cloudwatch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-aws-cdk-vpc-alb-ami-s3-cloudfront-cloudwatch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraAwsCdkVpcAlbAmiS3CloudfrontCloudwatch.InfraAwsCdkVpcAlbAmiS3CloudfrontCloudwatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
