91e14dbb1b67f3cdb43f90ce7c9353d4aec4471d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-28 00:43:36 +0000

    Integrate DestroyStack for enhanced resource cleanup in AWS CDK application
    - Added a new DestroyStack instance to facilitate the deletion of AWS resources with a specified prefix in the Tokyo region.
    - Improved the main stack file to include the DestroyStack, promoting better management of resource cleanup processes.
    - Ensured that the DestroyStack utilizes the same account settings as the main stack for consistency in deployment.
    
    These changes contribute to a more efficient and organized infrastructure management workflow.

--- IaC/infra-aws-cdk-vpc-alb-ami-s3-cloudfront/bin/infra-aws-cdk-vpc-alb-ami-s3-cloudfront.ts
+++ IaC/infra-aws-cdk-vpc-alb-ami-s3-cloudfront/bin/infra-aws-cdk-vpc-alb-ami-s3-cloudfront.ts
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { InfraAwsCdkVpcAlbAmiS3CloudfrontStack } from '../lib/infra-aws-cdk-vpc-alb-ami-s3-cloudfront-stack';
+import { DestroyStack } from '../lib/destroy-stack';
 
 const app = new cdk.App();
 new InfraAwsCdkVpcAlbAmiS3CloudfrontStack(app, 'InfraAwsCdkVpcAlbAmiS3CloudfrontStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
+  }
+});
+
+new DestroyStack(app, 'DestroyStack', {
+  prefix: 'cdk-express-01',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'ap-northeast-1'
   }
 });
